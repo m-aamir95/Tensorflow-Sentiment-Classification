ab1e10c9938fc42a2e4aa00cb01d2b05f05f2667
Aamir <djzaamir@gmail.com>
2022-11-05 12:34:01 +0500

    Added some explanation comments for the tensorflow toeknizer

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ if __name__ == '__main__':
     datasetSolver = LocalDriveDatasetSolver()
     base_dir_path = datasetSolver.resolve_dataset_dir()
 
-    sentences = ["I love my dog", "I love my cat"]
+    sentences = ["I love my dog", "i love my cat", "You LOVE my dog!"]
 
     # num_words, will take the top most N words and only encode them
+    # Sometimes tokenization and encoding all the word might not improve the accuracy much
+    # But can badly effect the training performance because of more computation needed
+    # Tokenizer class will also kind of normalize the text e.g removing punctuation, lowercase handling etc.
     tokenizer = Tokenizer(num_words=100)
     tokenizer.fit_on_texts(sentences)
 
