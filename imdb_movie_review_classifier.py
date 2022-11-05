e9684de5fa8ec14d622f3c809584f9e78489ab83
Aamir <djzaamir@gmail.com>
2022-11-05 14:21:41 +0500

    Wrote a simple NN for classifying imdb dataset

--- imdb_movie_review_classifier.py
+++ imdb_movie_review_classifier.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import tensorflow as tf
 from tensorflow import keras
 import tensorflow_datasets as tfds
@@ -5,5 +7,60 @@ import tensorflow_datasets as tfds
 from keras_preprocessing.text import Tokenizer
 from keras_preprocessing.sequence import pad_sequences
 
+
+def extract_sentences_and_labels_into_numpy_arrays(data_iterable):
+    sentences = []
+    labels = []
+    for text, label in data_iterable:
+        sentences.append(text.numpy().decode("utf8"))
+        labels.append(label.numpy())
+
+    return np.array(sentences), np.array(labels)
+
+
+def generate_padded_sequences(sentences, tokenizer, max_seq_length, trunc_type):
+    # Generating sequences from provided sentence data
+    seqs = tokenizer.texts_to_sequences(sentences)
+    # Apply padding
+    return pad_sequences(seqs, maxlen=max_seq_length, truncating=trunc_type)
+
+
 if __name__ == "__main__":
-    print("Hello World")
+    # Loading imdb movie review dataset
+    imdb, info = tfds.load("imdb_reviews", with_info=True, as_supervised=True)
+    train_data, test_data = imdb["train"], imdb["test"]
+
+    # Extracting the text part and the label part from both train and test sets
+    train_sentences, train_labels = extract_sentences_and_labels_into_numpy_arrays(train_data)
+    test_sentences, test_labels = extract_sentences_and_labels_into_numpy_arrays(test_data)
+
+    # Hyper parameters
+    vocab_size = 10000
+    embedding_dim = 16
+    max_seq_length = 120
+    trunc_type = "post"
+    oov_tok = "<OOV>"
+    num_epocs = 10
+
+    # Fit the tokenizer TODO; optimization; Tokenizer, Sequencer, Padder can be multi-threaded, tensorflow probably
+    #  already does it and we need to find the method which do it
+    tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+    tokenizer.fit_on_texts(train_sentences)
+
+    train_padded_sequences = generate_padded_sequences(train_sentences, tokenizer, max_seq_length, trunc_type)
+    test_padded_sequences = generate_padded_sequences(test_sentences, tokenizer, max_seq_length, trunc_type)
+
+    # Defining the neural network with a trainable embedding layer
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_seq_length),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(6, activation="relu"),
+        tf.keras.layers.Dense(1, activation="sigmoid")
+    ])
+
+    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+    model.fit(train_padded_sequences,
+              train_labels,
+              epochs=num_epocs,
+              validation_data=(test_padded_sequences, test_labels))
