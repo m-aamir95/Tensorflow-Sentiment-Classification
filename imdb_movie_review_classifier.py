0e21c15a73ca5b8f98f40902a241d3968a988a09
Aamir <djzaamir@gmail.com>
2022-11-05 13:36:35 +0500

    Paused Working on Sarcasm classification, switched to imdb movie review classification

---
+++ imdb_movie_review_classifier.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow_datasets as tfds
+
+from keras_preprocessing.text import Tokenizer
+from keras_preprocessing.sequence import pad_sequences
+
+if __name__ == "__main__":
+    print("Hello World")
--- sarcasm_classifier.py
+++ sarcasm_classifier.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
+import tensorflow_datasets as tfds
 from keras_preprocessing.text import Tokenizer
 from keras_preprocessing.sequence import pad_sequences
 
@@ -12,7 +13,7 @@ if __name__ == '__main__':
     base_dir_path = datasetSolver.resolve_dataset_dir()
 
     (article_links, article_headlines, article_ground_truths) = load_dataset(base_dir_path,
-                                                                                "Sarcasm_Headlines_Dataset.json")
+                                                                             "Sarcasm_Headlines_Dataset.json")
 
     # num_words, will take the top most N words and only encode them
     # Sometimes tokenization and encoding all the word might not improve the accuracy much
@@ -25,10 +26,8 @@ if __name__ == '__main__':
     # Can only provide token indices for the words which it had seen during fitting the call fit_on_texts
     sequences = tokenizer.texts_to_sequences(article_headlines)
 
-    # Apply padding
-    # Padding pre -> applies padding before the original seq
-    # Padding post -> applies padding after the original seq
-    # Max-length restricts the length of the sequence, else it maps all the seqs according to the longest original present in the corpus
-    # Truncating -> specifies incasse of truncation where should the truncation start
+    # Padding pre -> applies padding before the original seq Padding post -> applies padding after the original seq
+    # Max-length restricts the length of the sequence, else it maps all the seqs according to the longest original
+    # present in the corpus Truncating -> specifies in case of truncation where should the truncation start
     padded_seqs = pad_sequences(sequences, padding="post")
     print(padded_seqs.shape)
